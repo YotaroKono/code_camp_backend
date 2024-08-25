02a23a4da41ee96b6dddbfc651d98202a2e2a786
YotaroKono <yotaro20021210@ezweb.ne.jp>
2024-08-25 11:09:40 +0900

    ✨ APIの実装

--- app/main.py
+++ app/main.py
@@ -1,8 +1,30 @@
-from fastapi import FastAPI
+from http.client import HTTPException
+from app import cruds
+from app.database import ENGINE, Base, get_db
+from app.models import Diagnosis, Result
+from app.schemas import DiagnosisCreate, DiagnosisResult
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+
 
 app = FastAPI()
 
+@app.on_event("startup")
+def startup():
+    Base.metadata.drop_all(bind=ENGINE)
+    Base.metadata.create_all(bind=ENGINE)
 
 @app.get("/")
-async def root():
+def root():
     return {"message": "Hello World"}
+
+@app.post("/diagnosis/create", response_model=DiagnosisResult)
+def diagnosis_create(diagnosis: DiagnosisCreate, db: Session = Depends(get_db)):
+    return cruds.create_diagnosis(db=db, diagnosis=diagnosis)
+
+# @app.get('/diagnosis/{result_id}', response_model=Result)
+# def read_result(result_id: int, db: Session = Depends(get_db)):
+#     db_result = cruds.get_result(db, result_id=result_id)
+#     if not db_result:
+#         raise HTTPException(status_code=404, detail='Todo not found')
+#     return db_result
